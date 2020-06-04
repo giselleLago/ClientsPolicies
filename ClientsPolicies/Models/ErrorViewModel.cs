880fb448bf5d9710837e038007cf9966fef62462
Giselle <gisellelago25994@gmail.com>
2020-06-04 17:50:03 +0200

    ASP.NetCore MVC Web Application.

---
+++ ClientsPolicies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClientsPolicies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
