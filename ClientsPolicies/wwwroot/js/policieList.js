6393f0e8b195533c110554489dfcf047eb0f679d
Giselle <gisellelago25994@gmail.com>
2020-06-08 09:49:52 +0200

    Add js file to PolicieList

---
+++ ClientsPolicies/wwwroot/js/policieList.js
@@ -0,0 +1,38 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/admin/getall/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "id", "width": "20%" },
+            { "data": "amountInsured", "width": "20%" },
+            { "data": "email", "width": "20%" },
+            { "data": "inceptionDate", "width": "20%" },
+            { "data": "installmentPayment", "width": "20%" },
+            { "data": "clientId", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/Admin/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                            Edit
+                        </a>
+                        &nbsp;
+                        </div>`;
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
