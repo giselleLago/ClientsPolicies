61000fad54ba7b3a8de32fbd5e827ad378fbb413
Giselle <gisellelago25994@gmail.com>
2020-06-08 09:46:19 +0200

    Add Clients Model

---
+++ ClientsPolicies/Models/Clients.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientsPolicies.Models
+{
+    public class Clients
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
