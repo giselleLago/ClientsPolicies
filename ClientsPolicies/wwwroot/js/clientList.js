d0611826ef6c34459732ced5c8f5e34ef3777327
Giselle <gisellelago25994@gmail.com>
2020-06-15 17:50:16 +0200

    Change funtions in dataTable. clientList.js

--- ClientsPolicies/wwwroot/js/clientList.js
+++ ClientsPolicies/wwwroot/js/clientList.js
@@ -12,55 +12,10 @@ function loadDataTable() {
             "datatype": "json"
         },
         "columns": [
-            { "data": "id", "width": "20%" },
-            { "data": "name", "width": "20%" },
-            { "data": "email", "width": "20%" },
-            { "data": "role", "width": "20%" },
-            {
-                "data": "id",
-                "render": function (data) {
-                    return `<div class="text-center">
-                        <a href="/Clients/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
-                            Edit
-                        </a>
-                        &nbsp;
-                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
-                            onclick=Delete('/Clients/Delete?id='+${data})>
-                            Delete
-                        </a>
-                        </div>`;
-                }, "width": "40%"
-            }
-        ],
-        "language": {
-            "emptyTable": "no data found"
-        },
-        "width": "100%"
-    });
-}
-
-function Delete(url) {
-    swal({
-        title: "Are you sure?",
-        text: "Once deleted, you will not be able to recover",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true
-    }).then((willDelete) => {
-        if (willDelete) {
-            $.ajax({
-                type: "DELETE",
-                url: url,
-                success: function (data) {
-                    if (data.success) {
-                        toastr.success(data.message);
-                        dataTable.ajax.reload();
-                    }
-                    else {
-                        toastr.error(data.message);
-                    }
-                }
-            });
-        }
+            { "data": "id", "width": "25%" },
+            { "data": "name", "width": "25%" },
+            { "data": "email", "width": "25%" },
+            { "data": "role", "width": "25%" },
+        ]
     });
 }
