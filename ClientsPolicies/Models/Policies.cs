59d8f62f635dd79e146941683da28dc4d2598aec
Giselle <gisellelago25994@gmail.com>
2020-06-24 18:23:32 +0200

    Add Policies Model.

---
+++ ClientsPolicies/Models/Policies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientsPolicies.Models
+{
+    public class Policies
+    {
+        public string Id { get; set; }
+        public double AmountInsured { get; set; }
+        public string Email { get; set; }
+        public string InceptionDate { get; set; }
+        public bool InstallmentPayment { get; set; }
+        public string ClientId { get; set; }
+    }
+}
