66db76571b5f7d7579447f823597f93d6b37c69a
Giselle <gisellelago25994@gmail.com>
2020-06-29 19:04:28 +0200

    Change url in piliciesList.

--- ClientsPolicies/wwwroot/js/policieList.js
+++ ClientsPolicies/wwwroot/js/policieList.js
@@ -7,17 +7,17 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#DT_load').DataTable({
         "ajax": {
-            "url": "/admin/getall/",
+            "url": "/Policies/getall/",
             "type": "GET",
             "datatype": "json"
         },
         "columns": [
-            { "data": "id", "width": "20%" },
-            { "data": "amountInsured", "width": "20%" },
-            { "data": "email", "width": "20%" },
-            { "data": "inceptionDate", "width": "20%" },
-            { "data": "installmentPayment", "width": "20%" },
-            { "data": "clientId", "width": "20%" },
+            { "data": "id", "width": "18%" },
+            { "data": "amountInsured", "width": "18%" },
+            { "data": "email", "width": "18%" },
+            { "data": "inceptionDate", "width": "18%" },
+            { "data": "installmentPayment", "width": "18%" },
+            { "data": "clientId", "width": "18%" },
             
         ]
     });
